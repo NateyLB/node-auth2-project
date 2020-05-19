030aa44c45f505fff58aadf5a57e32ad67a2bf20
Nathan <nateyl.b@gmail.com>
2020-05-19 16:15:39 -0700

    moved some validation into its own middlware

--- users/users-router.js
+++ users/users-router.js
@@ -5,23 +5,31 @@ var jwtDecode = require('jwt-decode');
 const Users = require("./users-model.js");
 
 
-router.get("/", (req, res)=>{
-    var decoded = jwtDecode(req.headers.token)
-    Users.findBy({"users.username": decoded.username})
-    .then(([user])=>{
-        if(user.loggedIn === 1){
-            Users.find().where("users.department", user.department)
+router.get("/", isLoggedIn, (req, res)=>{       
+            console.log(req.user)
+            Users.find().where("users.department", req.user.department)
             .then(users=>{
                 res.status(200).json({ data: users})
             })
             .catch(err=>{
                 res.status(500).json({message: "Could not get users"})
             })
-        }else{
+})
+
+function isLoggedIn(req, res, next){
+    var decoded = jwtDecode(req.headers.token)
+    Users.findBy({"users.username": decoded.username})
+    .then(([user])=>{
+        if(user.loggedIn === 1){
+            req.user = user;
+            next()
+        }  else{
             res.status(500).json({ message: "Please login"})
         }
-    })
 })
+    
+}
+
 
 
 
